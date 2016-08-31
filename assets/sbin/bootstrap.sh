ba948b5e1ceaf2710a0a78f8c24668cfd39d18c7
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-08-31 22:18:17 +0100

    Make sure directories are created

--- assets/sbin/bootstrap.sh
+++ assets/sbin/bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p /etc/apt-cacher-ng
 chown -R root:root /etc/apt-cacher-ng
 chmod 644 /etc/apt-cacher-ng/*
 
+mkdir -p /var/{cache,log}/apt-cacher-ng
 chown -R $SYSTEM_USER:$SYSTEM_USER \
     /var/cache/apt-cacher-ng \
     /var/log/apt-cacher-ng
